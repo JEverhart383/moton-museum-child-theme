9fc82fa1dd8f4e2f5c1c403063d99a86e9bed8b9
Jeff Everhart <jeffeverhart383@gmail.com>
2020-02-15 15:04:12 -0500

    small tweak to rent button

--- front-page.php
+++ front-page.php
@@ -88,7 +88,7 @@ $container = get_theme_mod( 'understrap_container_type' );
             <p>Host your event at Virginia’s civil rights National Historic Landmark.</p>
             <p>The Moton Museum offers a fully restored historic space that is available to rent for private, public and non-profit functions.</p>
             <p>Our multi-purpose auditorium can accommodate up to 200 guests theatre style, and 100 guests banquet style, with a variety of A/V set-up options.</p>
-            <a href="/events/rental" class="btn btn-primary">Learn About Renting</a>
+            <a href="/events/rental" class="btn btn-primary mb-3">Learn About Renting</a>
           </div>
           <div class="col-lg-6">
             <img src="<?php echo get_stylesheet_directory_uri() . '/images/Moton-Auditorium.png'; ?>" alt="" class="fluid">
