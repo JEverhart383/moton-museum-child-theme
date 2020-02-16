8839144531175eb84b5c2bde7bec33351e59092c
Jeff Everhart <jeffeverhart383@gmail.com>
2020-02-16 12:02:12 -0500

    only call card resize when window.width is larger than small

--- js/child-theme.js
+++ js/child-theme.js
@@ -7054,13 +7054,20 @@ window.addEventListener('resize', setHeightToTallestCard);
 
 function setHeightToTallestCard() {
   var cards = document.querySelectorAll('.child-card-container .card');
-  var tallestCard = 0; 
-  for (var i = 0; i < cards.length; i++) {
-    if (cards[i].offsetHeight > tallestCard) {
-      tallestCard = cards[i].offsetHeight;
+  if (window.innerWidth > 768) {
+    var tallestCard = 0; 
+    for (var i = 0; i < cards.length; i++) {
+      if (cards[i].offsetHeight > tallestCard) {
+        tallestCard = cards[i].offsetHeight;
+      }
+    }
+    for (var j = 0; j < cards.length; j++) {
+      cards[j].style.height = tallestCard + 'px';
+    }
+  } else {
+    for (var k = 0; k < cards.length; k++) {
+      cards[k].style.height = '';
     }
   }
-  for (var j = 0; j < cards.length; j++) {
-    cards[j].style.height = tallestCard + 'px';
-  }
+
 }
