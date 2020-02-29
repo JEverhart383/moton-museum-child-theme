550923caba3522eb018a13cca5a6fab338f11bb6
Jeff Everhart <jeffeverhart383@gmail.com>
2020-02-28 20:52:46 -0500

    remove text from affiliates page

--- about-template.php
+++ about-template.php
@@ -44,13 +44,6 @@ $container = get_theme_mod( 'understrap_container_type' );
     ?>
   <?php endwhile; // end of the loop. ?>
   <div class="container-fluid text-center">
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <p>The Moton Museum is proudly affiliated with the Virginia Association of Museums, the American Association for State and Local History, The American Alliance of Museums, and the Association of African American Museums.</p>
-        </div>
-      </div>
-    </div>
     <img src="<?php echo get_stylesheet_directory_uri() . '/images/motonmuseum_affiliate_banner.jpg'; ?>" alt="" class="fluid mt-3 mb-3">
   </div>
   <?php 
