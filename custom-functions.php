9f00e1f8e7d7e1e8f4e9dfb9b1b25545e35214a8
Colin Werth <colin@letterpresscommunications.com>
2020-07-14 17:51:31 -0400

    Custom Button Text
    Card button text is now pulled from "custom_button_text" field.

--- custom-functions.php
+++ custom-functions.php
@@ -70,10 +70,12 @@ add_filter( 'pre_get_posts', 'moton_filter_pre_get_posts' );
 
 add_post_type_support( 'page', 'excerpt' );
 
+$button = get_field( "custom_button_text" );
+
 function replace_excerpt_button_class($text) {
 	$text = str_replace('<p>', '<p class="mt-auto">', $text);
 	$text = str_replace('btn-secondary', 'btn-primary', $text);
-	$text = str_replace('Read More...', 'Read More', $text);
+	$text = str_replace('Read More...', $button, $text);
 	$text = str_replace('[...]', '', $text);
 	return $text;
 }
