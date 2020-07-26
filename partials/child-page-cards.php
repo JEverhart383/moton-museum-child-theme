faf64c5264e0a744381355b2c987e240ad8761e4
Jeff Everhart <jeffeverhart383@gmail.com>
2020-07-26 13:53:02 -0400

    add custom field for custom child page button text

--- partials/child-page-cards.php
+++ partials/child-page-cards.php
@@ -17,7 +17,14 @@ $child_page_query = new WP_Query(array(
           <img src="<?php echo get_the_post_thumbnail_url(get_the_ID());?>" alt="<?php echo get_post_meta( get_post_thumbnail_id(get_the_ID()), '_wp_attachment_image_alt', true) ?>" class="card-img-top mh-225">
           <div class="card-body d-flex flex-column">
             <h5 class="card-title"><?php echo get_the_title();?></h5>
-            <?php echo get_the_excerpt();?>
+            <?php
+              $custom_button_text = get_field('custom_button_text', get_the_ID());
+              $exceprt = get_the_excerpt();
+              if($custom_button_text){
+                $exceprt = str_replace('Read More', $custom_button_text, $exceprt);
+              }
+              echo $exceprt;
+            ?>
           </div>
         </div>
     </div>
